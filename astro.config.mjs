b344279d88bed1e11319acd49335323383adc386
Andrew Meyer <andrewmeyer@granular.ag>
2024-10-13 11:26:17 -0500

    Adds theme chooser

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://rewdy.lol",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react()],
   vite: {
     css: {
       preprocessorOptions: {
---
+++ src/components/ThemeManager.tsx
@@ -0,0 +1,67 @@
+import type React from "react";
+import { useEffect } from "react";
+import { useLocalStorageState } from "ahooks";
+
+import "./ThemeManager.scss";
+
+type Season = "winter" | "spring" | "summer" | "autumn";
+const seasons: Season[] = ["winter", "spring", "summer", "autumn"];
+const seasonEmojis: Record<Season, string> = {
+  winter: "❄️",
+  spring: "🌱",
+  summer: "🌞",
+  autumn: "🍂",
+};
+
+export const ThemeManager: React.FC = () => {
+  const [themeSeason, setThemeSeason] = useLocalStorageState<
+    Season | undefined
+  >("rewdy.lol-theme", {
+    defaultValue: undefined,
+  });
+  const month = new Date().getMonth();
+  const currentSeason = seasons[Math.floor(((month + 1) % 12) / 3)];
+
+  const season = themeSeason || currentSeason;
+  const themeClass = `theme-${season}`;
+
+  const handleSetTheme = (season: Season) => {
+    if (themeSeason === season) {
+      setThemeSeason(undefined);
+    } else {
+      setThemeSeason(season);
+    }
+  };
+
+  useEffect(() => {
+    for (const cls of document.body.classList) {
+      if (cls.startsWith("theme-")) {
+        document.body.classList.remove(cls);
+      }
+    }
+    document.body.classList.add(themeClass);
+  }, [themeClass]);
+
+  return (
+    <ul className="inline-list" style={{ justifyContent: "center" }}>
+      <li className="text-small">Theme: </li>
+      {seasons.map((s) => {
+        const classes: string[] = [s];
+        if (s === season) classes.push("active-theme");
+        if (themeSeason) classes.push("custom");
+        return (
+          <li key={s} className={classes.join(" ")}>
+            <button
+              type="button"
+              title={`Set theme to ${s}`}
+              className="btn btn-text"
+              onClick={() => handleSetTheme(s)}
+            >
+              {seasonEmojis[s]}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
